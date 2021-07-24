0bf1e1d9986f00f51d01d87c6916b9a97b6ac549
Robert Kregloh <gxrobb@gmail.com>
2021-07-24 18:00:07 -0400

    Job Slider Working Somewhat

---
+++ components/EmployerDescription.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function EmployerDescription({ jobObject }) {
+  return (
+    <>
+      <h3>{jobObject.title}</h3>
+      <h4>{jobObject.date}</h4>
+        <ul>
+          { jobObject.description.map((value) => {return <li key="value">{value}</li>})}
+        </ul>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,86 @@
+import React, { useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 import profilePic from '../public/images/me.jpg'
 import ProjectCard from '../components/ProjectCard.js'
+import EmployerDescription from '../components/EmployerDescription.js'
 
-export default function Home() {
+export default function Home() { 
+  const jobList = [
+    {
+        id: 1,
+        label: 'Pool.Garden',
+        title: 'Front-End Developer @ Pool.Garden',
+        date: 'May 2021 - Present',
+        description: [
+        'I work with the Back-End developer to deliver a service for cryptocurrency miners to combine their efforts in a pool.',
+        'Responsibilities include maintaining the front end codebase, adding new features and designing any features or elements as they are needed.',   
+        'Some of the skills I use in this position include NextJS, SASS, HTML.'
+        ]
+    },
+    {
+        id: 2,
+        label: 'Firewalls.com',
+        title: 'Senior Front-End Developer @ Firewalls.com',
+        date: 'August 2019 - Present',
+        description: [
+        'I am currently developing on Magento 2, Wordpress and Laravel to maintain a online firewall reseller website. In addition to development, I also assist the marketing team with the ocasional web banner.',
+        'Part of my responsabilities include managing 3 other developers, deploying code bi-weekly via Bitbucket Pipelines and maintaining the internal API.',   
+        'Some of the skills I use in this position include PHP, Laravel, Magent 2 (Zend Framework), Vue3, LESS, SASS, HTML.'
+        ]
+    },
+    {
+      id: 3,
+      label: 'BitLoft',
+      title: 'Front-End Developer @ BitLoft',
+      date: 'August 2016 - August 2019',
+      description: [
+      'While at BitLoft, I wore various hats, as a startup, I had the pleasure of working on both Front-End and Back-End projects. While the majority of my time was spent designing and developing for internal projects as well as agency clients, I also had a chance to expand my skills in some marketing and technology related areas.',
+      'I worked on projects from small wordpress sites, to large laravel SaaS apps. I also assited the marketing director with planning in-person events at conventions, assisted the esports director on various events and projects ranging from configuring television monitors to display ads and create video tralers for upcoming competitions.',   
+      'Some of the web specific skills I used during my time at BitLoft include Laravel 5, PHP, CSS, SASS, LESS, JavaScript, jQuery, MySQL, Google Cloude Platform, Design and Print Design.'
+      ]
+    },
+    {
+      id: 4,
+      label: '4 Media',
+      title: 'Web Designer @ 4 Media',
+      date: 'February 2015 - May 2016',
+      description: [
+      'While at 4 Media, I spent most of my time honing my developer skills. I created over 100 bootstrap websites for various different vendors, small 1 page websites. I ocasionally got more complex projects like landing pages which required a bit of JavaScript.',
+      'My responsibilities here ranged from graphic design, to quick turn over web design and development for fast paced clients.',   
+      'Some of the skills I use in this position include PHP, CSS, LESS, Twitter Bootstrap, jQuery, JavaScript.'
+      ]
+    },
+    {
+      id: 5,
+      label: 'JC Marketing',
+      title: 'Graphic Designer @ JC Marketing',
+      date: 'July 2014 - August 2016',
+      description: [
+      'At JC Marketing, I assisted the marketing director with graphic assets such as banners, buttons, magazine pages and things of that nature',
+      'The majority of my work here consisted of graphical asset requests and a timely creation of the assets. Once in a while, I was asked to assist on some smaller web development projects.',   
+      'Some of the skills I use in this position include Design, Photoshop, Illustrator, Indesign, HTML, CSS, jQuery, JavaScript'
+      ]
+    }
+  ]
+
+
+  const [activeEmployer, setActiveEmployer] = useState(jobList[0]);
+
+  function EmployerActivator(value){
+    setActiveEmployer(value);
+    console.log(value)
+  }
+
+  function ListAllTitles(data){
+      return (
+        <>
+          {data.data.map((value) => {return <li onClick={ () => EmployerActivator(value) }  className={activeEmployer.id == value.id ? styles.active : null} key="id"> {value.label} </li>})}
+        </>
+      )
+    }
+    
   return (
     <div>
       <Head>
@@ -86,21 +162,12 @@ export default function Home() {
           <div className={styles.experienceGrid}>
             <div className={styles.employer}>
               <ul>
-                <li className={styles.active}>Pool.Garden</li>
-                <li>Firewalls.com</li>
-                <li>BitLoft</li>
-                <li>4Media</li>
-                <li>JC Marketing</li>
+                <ListAllTitles data={jobList} />
               </ul>
             </div>
             <div className={styles.employerDescription}>
-              <h3>Front-End Developer @ Pool.Garden</h3>
-              <h4>May 2021 - Present</h4>
-              <ul>
-                <li>I work with the Back-End developer to deliver a service for cryptocurrency miners to combine their efforts in a pool.</li>
-                <li>Responsibilities include maintaining the front end codebase, adding new features and designing any features or elements as they are needed.</li>
-                <li>Some of the skills I use in this position include NextJS, SASS, HTML.</li>
-              </ul>
+            
+              <EmployerDescription jobObject={activeEmployer}/>
             </div>
           </div>
         </section>
