a9a6ef148dc83830727a24cb649011ec5b6a60d5
Robert Kregloh <gxrobb@gmail.com>
2025-08-06 16:11:07 -0400

    more cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,15 @@
-import { useState } from 'react'
-import Head from 'next/head'
-import styles from '../styles/Home.module.scss'
-import ExperienceSection from '../components/ExperienceSection.js'
-import { LIST_OF_EMPLOYERS } from '../constants/jobs.js'
-import { AboutSection } from '../components/AboutSection.js'
-import SideIcons from '../components/SideIcons.js'
-import Navigation from '../components/Navigation.js'
+import { useState } from 'react';
+import Head from 'next/head';
+import styles from '../styles/Home.module.scss';
+import ExperienceSection from '../components/ExperienceSection.js';
+import { LIST_OF_EMPLOYERS } from '../constants/jobs.js';
+import { AboutSection } from '../components/AboutSection.js';
+import SideIcons from '../components/SideIcons.js';
+import Navigation from '../components/Navigation.js';
 
-
-export default function Home() { 
-
+export default function Home() {
   const [activeEmployer, setActiveEmployer] = useState(LIST_OF_EMPLOYERS[0]);
-    
+
   return (
     <>
       <Head>
@@ -27,35 +25,52 @@ export default function Home() {
           <div className={styles.heroWrapper}>
             <p>Oh, Hello there! My name is</p>
             <h1>ROBERT KREGLOH</h1>
-            <h3>And I like to build <span className={styles.highlight}>useful</span> things!</h3>
-            <p>I am a <span className={styles.highlight}>Senior Front-End Web Developer</span> who has a passion for graphic and interaction design.</p>
-            <a href="mailto:gxrobb@gmail.com"><button>Get In Touch</button></a>
+            <h3>
+              And I like to build{' '}
+              <span className={styles.highlight}>useful</span> things!
+            </h3>
+            <p>
+              I am a{' '}
+              <span className={styles.highlight}>
+                Senior Front-End Web Developer
+              </span>{' '}
+              who has a passion for graphic and interaction design.
+            </p>
+            <a href="mailto:gxrobb@gmail.com">
+              <button>Get In Touch</button>
+            </a>
           </div>
-
-        </section> 
+        </section>
 
         <AboutSection />
 
-        <ExperienceSection 
-          activeEmployer={activeEmployer} 
-          setActiveEmployer={setActiveEmployer} 
+        <ExperienceSection
+          activeEmployer={activeEmployer}
+          setActiveEmployer={setActiveEmployer}
         />
-        
 
         <div id="contact" />
-        <section className={`${styles.copySection} ${styles.contact} container`}>
+        <section
+          className={`${styles.copySection} ${styles.contact} container`}
+        >
           <div className={styles.title}>
             <h2>Contact</h2>
           </div>
           <div className={styles.contactWrapper}>
-              <p>If you have any questions, or just want to say hi, feel free to shoot me an email! Click the button below to launch your preferred email client!</p>
-              <a href="mailto:gxrobb@gmail.com"><button>Email Me</button></a>
-            </div>
+            <p>
+              If you have any questions, or just want to say hi, feel free to
+              shoot me an email! Click the button below to launch your preferred
+              email client!
+            </p>
+            <a href="mailto:gxrobb@gmail.com">
+              <button>Email Me</button>
+            </a>
+          </div>
         </section>
       </main>
       <footer className={styles.footer}>
         <p>Designed And Developed By Robert Kregloh | Deployed On Netlify!</p>
       </footer>
     </>
-  )
+  );
 }
