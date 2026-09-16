79cf3b26276a1b1e68fbb2e52daa5fe4f2e46476
Robert Kregloh <gxrobb@gmail.com>
2026-09-16 10:13:25 -0400

    Merge pull request #3 from gxrobb/update-deps-and-cleanup
    Update deps and cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,5 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.scss'
-import '@fortawesome/fontawesome-free/js/fontawesome';
-import '@fortawesome/fontawesome-free/js/solid';
-import '@fortawesome/fontawesome-free/js/regular';
-import '@fortawesome/fontawesome-free/js/brands';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
